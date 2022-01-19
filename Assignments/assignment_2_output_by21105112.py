08a35b2fe0092d4b2784026e9f9ea8b5062a99df
Tarush <96109309+pprachuu@users.noreply.github.com>
2022-01-19 12:04:52 +0530

    Add files via upload

---
+++ Assignments/assignment_2_output_by21105112.py
@@ -0,0 +1,91 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: E:\Computing\Assignments\assignment2_q1.py =============
+Program Number 1 
+ 
+
+a part  
+
+35 
+
+b part 
+
+egaugnal evitisnes esac a si nohtyP 
+
+c part 
+
+a case sensitive  
+
+d part 
+
+Python is object oriented language 
+
+e part 
+
+10 
+
+f part 
+
+Pythonisacasesensitivelanguage 
+
+Program Number 2 
+ 
+
+Enter your Name Tarush
+Enter your SID 5112
+Enter your Department Name ECE
+Enter your CGPA 4.5
+ Hey Tarush Here! 
+ My SID is 21105112 
+ I am from ECE department and my CGPA is 4.5
+Program Number 3 
+ 
+
+a & b 
+
+8 
+
+a | b 
+
+58 
+
+a ^ b 
+
+50 
+
+Left shift with 2 bits 
+
+224 
+
+40 
+
+Right shift of a with 2 bits 
+
+14 
+
+Right shift of b with 4 bits 
+
+0 
+
+Program Number 4 
+ 
+
+Enter first number 12
+Enter second number 23
+Enter third number 13
+23 is greatest 
+
+Program Number 5 
+ 
+
+Enter the String You forgot his nickname
+Yes 
+
+Program Number 6 
+ 
+
+Enter the first side 13
+Enter the second side 14
+Enter the third side 22
+Yes
