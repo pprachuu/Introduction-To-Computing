ea42ab7adab0791f90480c1ab006276b60e222e4
Tarush <96109309+pprachuu@users.noreply.github.com>
2022-03-02 10:49:17 +0530

    Add files via upload

---
+++ Assignments/assignment_4_output_by21105112.py
@@ -0,0 +1,117 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: E:\Computing\Assignments\assignment4_by21105112.py =========
+1 
+
+ Solution the problem of tower of Hanoi with three disks: 
+Move Disk 1 from A to B
+Move Disk 2 from A to C
+Move Disk 1 from B to C
+Move Disk 3 from A to B
+Move Disk 1 from C to A
+Move Disk 2 from C to B
+Move Disk 1 from A to B
+
+
+2 
+
+Enter the number of rows in Pascal's Triangle: 12
+                      1   
+                    1   1   
+                  1   2   1   
+                1   3   3   1   
+              1   4   6   4   1   
+            1   5   10   10   5   1   
+          1   6   15   20   15   6   1   
+        1   7   21   35   35   21   7   1   
+      1   8   28   56   70   56   28   8   1   
+    1   9   36   84   126   126   84   36   9   1   
+  1   10   45   120   210   252   210   120   45   10   1   
+1   11   55   165   330   462   462   330   165   55   11   1   
+
+
+                      1   
+                    1   1   
+                  1   2   1   
+                1   3   3   1   
+              1   4   6   4   1   
+            1   5   10   10   5   1   
+          1   6   15   20   15   6   1   
+        1   7   21   35   35   21   7   1   
+      1   8   28   56   70   56   28   8   1   
+    1   9   36   84   126   126   84   36   9   1   
+  1   10   45   120   210   252   210   120   45   10   1   
+1   11   55   165   330   462   462   330   165   55   11   1   
+
+
+
+3 
+
+Enter the first integer: 14
+Enter the first integer: 3
+Remainder is:  2
+Quotient is:  4
+
+
+a 
+
+Function is Not-callable
+
+
+b 
+
+Both values are non zero
+
+
+c 
+
+Added (4,5,6) in the values (2,4)
+Values greater than 4 are 5,6
+
+
+d 
+
+Above result in set form is shown below:
+{5, 6}
+
+
+e 
+
+{5, 6}
+Immutable set:  frozenset({5, 6})
+
+
+f 
+
+Largest value in the set:  6
+Hash value:  6
+
+
+4 
+
+Object created
+The name of the student is Tarush and their SID is 21105112.
+Object destroyed
+
+
+5 
+
+a part
+The updated salary of the employee Mehak is 70000
+b part
+ The record of Employee Viren is deleted
+
+
+6 
+
+Enter the first word: kin
+
+
+Enter a new meaningful word to test your friendship: ink
+Does the word makes sense?(yes or no) yes
+
+
+The friends passed the friendship test
+
+
